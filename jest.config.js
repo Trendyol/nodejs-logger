708db83c3ff25b629ff0d46860f5f399e34aaf63
Ahmet Can Güven <ahmetcanguven44@gmail.com>
2019-05-07 16:27:02 +0300

    CircleCI integration & codecov

--- jest.config.js
+++ jest.config.js
@@ -12,13 +12,5 @@ module.exports = {
       diagnostics: false
     }
   },
-  setupFiles: ['<rootDir>/jestSetup.js'],
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80
-    }
-  },
+  setupFiles: ['<rootDir>/jestSetup.js']
 };
