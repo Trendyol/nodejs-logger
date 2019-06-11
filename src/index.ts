c735039f994b7d544df30cc24dfd6e33bda2fc76
İsmail Hakkı TEKİN <ismail.tekin@trendyol.com>
2019-06-11 15:37:12 +0300

    Export LogContext, Request and GraylogConfig interfaces

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { ConsoleAdapter } from './adapters/console-adapter';
 import { ExpressLogContextMiddleware } from './middlewares/express-log-context-middleware';
 import { Logger } from './logger/logger';
 import { Action } from './actions/actions';
+import { LogContext, Request, GraylogConfig } from './types';
 
-export { GraylogAdapter, ConsoleAdapter, ExpressLogContextMiddleware, Logger, Action };
+export { GraylogAdapter, ConsoleAdapter, ExpressLogContextMiddleware, Logger, Action, LogContext, Request, GraylogConfig };
