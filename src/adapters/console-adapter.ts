1e7826b503a36931e4b73eef1bd21efb7b5ade68
cihad.horuzoglu <cihad.horuzoglu@trendyol.com>
2021-02-17 13:38:37 +0300

    customAction removed and console log level added

--- src/adapters/console-adapter.ts
+++ src/adapters/console-adapter.ts
@@ -1,28 +1,28 @@
-import { Adapter, AdapterLog } from '../types';
+import { Adapter, AdapterLog, LogLevel } from '../types';
 
 class ConsoleAdapter implements Adapter {
   public info(log: AdapterLog) {
-    console.info(ConsoleAdapter.mergeLog(log));
+    console.info(ConsoleAdapter.mergeLog(log, LogLevel.info));
   }
 
   public error(log: AdapterLog) {
-    console.error(ConsoleAdapter.mergeLog(log));
+    console.error(ConsoleAdapter.mergeLog(log, LogLevel.error));
   }
 
   public warn(log: AdapterLog) {
-    console.warn(ConsoleAdapter.mergeLog(log));
+    console.warn(ConsoleAdapter.mergeLog(log, LogLevel.warn));
   }
 
   public debug(log: AdapterLog) {
-    console.debug(ConsoleAdapter.mergeLog(log));
+    console.debug(ConsoleAdapter.mergeLog(log, LogLevel.debug));
   }
 
-  public static mergeLog(log: AdapterLog) {
+  public static mergeLog(log: AdapterLog, level: LogLevel) {
     let message = log.message;
     if (typeof message === 'string') {
       message = { message };
     }
-    return JSON.stringify({ ...message, ...log.meta });
+    return JSON.stringify({ ...message, level, ...log.meta });
   }
 
   public validate() {
